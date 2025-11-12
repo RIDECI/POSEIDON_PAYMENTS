26e88db5a28e41aa91f8fba6b4c12fc25ab0e092
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-11-11 20:05:34 -0500

    Estructurado inicial

---
+++ src/main/java/edu/dosw/rideci/POSEIDON_PAYMENTS/PoseidonPaymentsApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.POSEIDON_PAYMENTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseidonPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/rideci/POSEIDON_PAYMENTS/PoseidonPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.POSEIDON_PAYMENTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
