4ea6f0e920c8b8fa8d39e0ec66c337d80607bb83
Oscar Sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-11-27 14:24:52 -0500

    Post_fix: correguir properties

--- src/main/java/edu/dosw/rideci/PoseidonPaymentsApplication.java
+++ src/main/java/edu/dosw/rideci/PoseidonPaymentsApplication.java
@@ -3,13 +3,9 @@ package edu.dosw.rideci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication
-
+@SpringBootApplication(scanBasePackages = "edu.dosw.rideci")
 public class PoseidonPaymentsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PoseidonPaymentsApplication.class, args);
     }
-
 }
