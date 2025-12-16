3f6749134fc797a20d19bd64d4cf2742b4f6b2de
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-12-16 00:15:36 -0500

    feat(core): implementacion cors

--- src/main/java/edu/dosw/rideci/infrastructure/config/CorsConfig.java
+++ src/main/java/edu/dosw/rideci/infrastructure/config/CorsConfig.java
@@ -14,9 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://rideci.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
